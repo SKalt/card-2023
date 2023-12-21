7e1aed15838b0d29e70dce8ebe67182771eed8b2
Steven Kalt <kalt.steven@gmail.com>
2023-12-21 10:56:17 -0500

    feat: add panorama

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import panorama from '$lib/assets/roof_panorama.jpg?enhanced'; 
+</script>
+<!-- <img src="{img}" alt="Panorama of a sunset from our roof in Philadelphia"> -->
+<enhanced:img src="{panorama}" class="panorama" alt="Panorama of a sunset from our roof in Philadelphia"/>
+<style>
+  .panorama {
+    height: 100%;
+    max-height: 100vh;
+  }
+  :global(body) {
+    margin: 0;
+  }
+</style>
+
+<!-- TODO: click-and-drag -->
+<!-- TODO: eliminate downward space in mobile -->
